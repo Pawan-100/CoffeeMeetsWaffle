abf702464ff34b585d15dcce9dd7d337c7efcbcf
Pawan-100 <pawanshimpi100@gmail.com>
2025-11-24 20:31:31 +0530

    feat: Add Credits page and update favicon.

---
+++ src/pages/Credits.jsx
@@ -0,0 +1,14 @@
+function Credits() {
+    return (
+        <div className="min-h-screen bg-white">
+            <h1>Following resources have been used in the project:</h1>
+            <ul>
+                <li>
+                    <a href="https://www.freepik.com/free-vector/hand-drawn-cafe-signage-design_22730194.htm#fromView=image_search_similar&page=1&position=17&uuid=8196823c-9421-4899-8582-0e97578f594f&query=coffee+cup">Image by freepik</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Credits;
